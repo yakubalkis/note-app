bc8279955fae62399cf9384d2cef55764ea8fa7b
yakubalkis <alkis.yakub@gmail.com>
2023-12-09 15:32:41 +0300

    fix: - added local storage reducer function to save redux data after refresh.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NoteList from './components/NoteList';
 import { Stack } from '@mui/material';
-import { useAppSelector } from './features/hooks';
+import { useAppDispatch, useAppSelector } from './features/hooks';
 import NoteForm from './components/NoteForm';
+import { setNotesFromLocalStorage } from './features/notesSlice';
 
 function App() {
   const isShowForm = useAppSelector(state => state.notesSlice.isShowForm);
-  
+  const dispatch = useAppDispatch();
+
+  useEffect(() => { // after refresh, this is invoked once time
+    dispatch(setNotesFromLocalStorage());
+  }, []);
+
   return (
     <Stack className="App" 
           display="flex"
