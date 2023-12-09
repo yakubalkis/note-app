3a6d902b8d7950598ca2d6588d25d6fe8d0b472d
yakubalkis <alkis.yakub@gmail.com>
2023-12-09 18:36:46 +0300

    feat: - added delete and update functions for note.

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,15 +1,28 @@
-import { Avatar, Box, Button, Divider, ListItem, ListItemAvatar, ListItemIcon, ListItemText, Stack, Typography } from "@mui/material";
+import { Avatar, Button, Divider, ListItem, ListItemAvatar, ListItemIcon, ListItemText, Stack, Typography } from "@mui/material";
 import PinIcon from "../img/pin-icon.png"; 
 import TrashIcon from "../img/icons-trash.png";
 import WriteIcon from "../img/icons-write.png";
-import { Note as NoteType } from "../features/notesSlice";
+import { Note as NoteType, deleteNote, setIsShowForm } from "../features/notesSlice";
+import { useAppDispatch } from "../features/hooks";
 
 
 const red = "#890000";
 const blue = "#001F89";
 const yellow = "#FDF604 ";
 
-export default function Note({title, content, color, date}: NoteType) {
+export default function Note({id, title, content, color, date}: NoteType) {
+    const dispatch = useAppDispatch();
+
+
+    function handleDelete(id:string) {
+        dispatch(deleteNote(id));
+    }
+
+    function handleUpdate(id:string) {
+       dispatch(setIsShowForm(true));
+       localStorage.setItem("updatedNoteId", id); 
+    }
+
     return(
         <>
         <ListItem>
@@ -25,10 +38,10 @@ export default function Note({title, content, color, date}: NoteType) {
             />
             <Typography sx={{position:"absolute", bottom: "0",opacity:"0.5",fontSize:"0.9rem"}}>{date}</Typography>
             <Stack direction="column" spacing={1} sx={{marginLeft: "5px"}}>
-                <Button variant="contained" color="error" size="small">
+                <Button variant="contained" color="error" size="small" onClick={() => handleDelete(id)}>
                     <img src={TrashIcon} />
                 </Button>
-                <Button variant="contained" color="success" size="small">
+                <Button variant="contained" color="success" size="small" onClick={() => handleUpdate(id)}>
                     <img src={WriteIcon} />
                 </Button>
             </Stack> 
